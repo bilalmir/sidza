e0fd540cb60d5050874949c942ba6aaddd332e63
Mir Bilal Ahmad <3272849+bilalmir@users.noreply.github.com>
2018-05-01 22:24:31 +0530

    Update TeacherProfile.aspx.cs

--- TeacherProfile.aspx.cs
+++ TeacherProfile.aspx.cs
@@ -11,7 +11,10 @@ namespace VirtualClassroom_final.UI
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if(session["email"]!=null)
+            {
+              lblusername.text="welcome" + t1.text;
+            }
         }
     }
 }
